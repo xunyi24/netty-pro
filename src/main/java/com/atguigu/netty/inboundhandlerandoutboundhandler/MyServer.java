4f6c343de1c0edd3e8ddce1c7cb37f86a2b6f728
xunyi24 <liupf24@163.com>
2022-02-23 08:44:30 +0800

    init proj

---
+++ src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyServer.java
@@ -0,0 +1,34 @@
+package com.atguigu.netty.inboundhandlerandoutboundhandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args) throws Exception{
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    // 自定义一个初始化类
+                    .childHandler(new MyServerInitializer());
+
+
+            ChannelFuture channelFuture = serverBootstrap.bind(7000).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
